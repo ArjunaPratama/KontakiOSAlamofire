cf95eb42b08c5c26a3ce0b4c8a2fdc49f432456d
ArjunaPratama <32661531+ArjunaPratama@users.noreply.github.com>
2017-11-13 11:48:53 +0700

    KontakiOSAlamofire

---
+++ KontakiOSAlamofire/KontakiOSAlamofire/KontakTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  KontakTableViewCell.swift
+//  KontakiOSAlamofire
+//
+//  Created by Jun  on 11/13/17.
+//  Copyright © 2017 Arjuna. All rights reserved.
+//
+
+import UIKit
+
+class KontakTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelEmail: UILabel!
+    @IBOutlet weak var labelNama: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
